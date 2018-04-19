64ba6b75c33aab3b2c4dadb25b9fb2718a98e753
gekko <dothanhcong.hust@gmail.com>
2018-04-19 10:37:33 +0700

    rm seperate_into_params, use params_stat.py instead

--- process_logs.py
+++ process_logs.py
@@ -1,6 +1,7 @@
 import os 
 import sys
-
+import operator 
+import time
 
 HOME_PATH = '/media/gekko/FA8A195BBBBEC20C/bk10-2017.2/'
 #HOME_PATH = 'D:\\bk10-2017.2\\'
@@ -11,6 +12,7 @@ count = 0
 def preprocess_logs():
 	global count 
 
+	pname_dict = dict()
 	spec_set = set()
 	for d in dirs:
 
@@ -19,10 +21,14 @@ def preprocess_logs():
 		files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
 		for filename in files :
-			
+			if filename == '.gitignore':
+				continue
 			log_set = set()
 			with open( path + filename, 'r') as f:
 				for l in f :
+					if len(l.split('"')) < 3:
+						print l 
+						exit()
 					request = l.split('"')[1]
 					#ip = l.split(' ')[0]
 					#response_code = l.split('"')[2]
@@ -32,8 +38,10 @@ def preprocess_logs():
 						spec_set.add(request + '\n')
 						continue 
 					#log_set.add(ip + ' ##' + response_code + '##' + request + '|,1\n')
+					
 					log_set.add(request + '\n')
-				print d, ':', filename + ' ====> done. Size set: ' + len(log_set) 
+
+				print d, ':', filename + ' ====> done. Size set: ', len(log_set) 
 				
 
 			f_out = open('preprocess_logs/' + d + '_' + filename, 'w')
@@ -41,7 +49,7 @@ def preprocess_logs():
 				f_out.write(item)
 
 			f_out.close()
-
+	
 	# store all request has '|' in URL
 	with open(HOME_PATH + "spec_req.txt", "w") as f_out:
 		for item in spec_set:
@@ -66,42 +74,24 @@ def generate_data():
 			f_out.write(item)
 
 
-def seperate_into_params():
-	param_set = set()
-	with open(FILE_OUT, "r") as f :
-		for line in f :
-			line = line.split(' ')
-			if len(line) < 2:
-				continue
-
-			l = line[1].split('?')
-			if len(l) < 2: 
-				print l 
-				continue
-			params = l[1].split('&')
-			param_set.update(params)
-	with open("param_set.txt", "w") as fout:
-		for item in param_set:
-			fout.write(item+"\n") 
-
-
 #REQUEST_METHODS = ['GET', 'POST', 'HEAD', 'PUT', 'DELETE', 'OPTIONS', 'CONNECT']
 
 
 if __name__ == '__main__' :
 	if len(sys.argv) == 1:
-		
+		start = time.time()
 		print '----------------- Preprocesss ----------------------\n'
-		#preprocess_logs()
+		preprocess_logs()
 		print "Number of line has '|': ", count 
 
 		print '----------------- Generate data ------------------------\n'
-		#generate_data()
+		generate_data()
 
 		print '--------------------- Statistic -----------------------------\n'
 		#data_statistic()
-
-		seperate_into_params()
+		
+		#seperate_into_params(FILE_OUT)
+		print "time", time.time() - start
 	elif len(sys.argv) == 2 :
 		if sys.argv[1] == '-h' or sys.argv[1] == '--help' or sys.argv[1] == '-help': 
 			print "Usage: "
